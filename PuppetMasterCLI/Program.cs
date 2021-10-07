8e567689b4170247f6b00bc8ae0c8b17c3be99eb
OcarinaRedcoat <ricardo.caetano.aleixo@tecnico.ulisboa.pt>
2021-10-07 23:41:37 +0100

    Added PuppetMaster Commands Merged with the Teacher code

--- PuppetMasterCLI/Program.cs
+++ PuppetMasterCLI/Program.cs
@@ -6,7 +6,82 @@ namespace PuppetMasterCLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Initializing the PuppetMasterCLI");
+      
+            while (true) { 
+               
+            }
         }
+        
+        static void ImportFile(string fileName)
+        {
+            // Input file name. Then each line is parsed according to the specs.
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            // Display the file contents by using a foreach loop.
+        
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                ParseConfigScriptLine(line);
+            }
+
+        }
+
+        static void ParseConfigScriptLine(string scritpLine)
+        {
+            stri
+            string[] configArgs = text.Split(' ');
+            string command = configArgs[0]
+            switch (command)
+            {
+                case "scheduler":
+                    CreateScheduler(configArgs[1], configArgs[2]);
+                    break;
+                case "storage":
+                    CreateStorage(configArgs[1], configArgs[2], Int32.Parse(configArgs[3]));
+                    break;
+                case "worker":
+                    CreateWorker(configArgs[1], configArgs[2], Int32.Parse(configArgs[3]));
+                    break;
+                case "client":
+                    ClientRequest(configArgs[1]);
+                    break;
+                case "populate":
+                    Populate(configArgs[1]);
+                    break;
+                case "status":
+                    Status();
+                    break;
+                case "listServer":
+                    ListServer(configArgs[1]);
+                    break;
+                case "listGlobal":
+                    ListGlobal();
+                    break;
+
+                case "debug":
+                    Debug();
+                    break;
+                case "crash":
+                    Crash(configArgs[1]);
+                    break;
+                case "wait_interval":
+                    Wait(Int32.Parse(configArgs[1]));
+                    break;
+            }
+        }
+
+        static void CreateScheduler(string serverId, string url) { }
+        static void CreateStorage(string serverId, string url, int gossipDelay) { }
+        static void CreateWorker(string serverId, string url, int gossipDelay) { }
+        static void ClientRequest(string inputAppFileName) { }
+        static void Populate(string dataFileName) { }
+        static void Status() { }
+        static void ListServer(string serverId) { }
+        static void ListGlobal() { }
+        static void Debug() { }
+        static void Crash(string serverId) { }
+        static void Wait(int waitInterval) { }
     }
 }
---
+++ StorageNode/IStorageNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DIDAStorage {
+	public interface IDIDAStorage {
+		DIDARecord Read(string id, DIDAVersion version);
+		DIDAVersion Write(string id, string val);
+		DIDAVersion UpdateIfValueIs(string id, string oldvalue, string newvalue);
+	}
+	public struct DIDARecord {
+		public string id;
+		public DIDAVersion version;
+		public string val;
+	}
+
+
+	public struct DIDAVersion {
+		public int versionNumber;
+		public int replicaId;
+	}
+}
