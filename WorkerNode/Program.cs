1a952b2bcdebddcf291250aaeef26ae67b8eebf9
vascofaria <vascoffaria@gmail.com>
2021-10-21 20:39:05 +0100

    minor changes

--- WorkerNode/Program.cs
+++ WorkerNode/Program.cs
@@ -11,11 +11,16 @@ namespace WorkerNode
             Console.WriteLine("Hello World from WorkerNode!");
             var host = args[1].Split("//")[1].Split(":")[0];
             var port = args[1].Split("//")[1].Split(":")[1];
-            var debug = Int32.Parse(args[2]);
+            var debug = Int32.Parse(args[2]).Equals(1);
             var logURL = args[3];
 
+            Console.WriteLine(Int32.Parse(args[2]).ToString(), logURL);
+
+            if (debug)
+                Console.WriteLine("Debug mode activated...");
+
             Console.WriteLine("Args: " + host + " : " + port);
-            WorkerServer workerServer = new WorkerServer(args[0], host, Int32.Parse(port), debug.Equals(1), logURL);
+            WorkerServer workerServer = new WorkerServer(args[0], host, Int32.Parse(port), debug, logURL);
 
             Console.ReadLine();
             workerServer.ShutDown();
