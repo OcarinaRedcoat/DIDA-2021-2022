bf5ec5fbb2f2c1739186d7296bb3436ce6d8deca
vascofaria <vascoffaria@gmail.com>
2021-11-06 22:56:33 +0000

    Separate Consitent HAshing from Proxy

--- WorkerNode/StorageProxy.cs
+++ WorkerNode/StorageProxy.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using DIDAStorageClient;
 using DIDAWorker;
-using System.Security.Cryptography;
 
 namespace WorkerNode
 {
@@ -37,7 +36,7 @@ namespace WorkerNode
                 storageIds.Add(n.serverId);
             }
             _meta = metaRecord;
-            _consistentHashing = new StorageProxy.ConsistentHashing(storageIds);
+            _consistentHashing = new ConsistentHashing(storageIds);
         }
 
         // THE FOLLOWING 3 METHODS ARE THE ESSENCE OF A STORAGE PROXY
@@ -54,7 +53,7 @@ namespace WorkerNode
             // TODO: How to choose the storage function?
             List<string> storagesIds = _consistentHashing.ComputeSetOfReplicas(r.Id);
             Console.WriteLine("Calling Read...");
-            foreach(string sId in storagesIds)
+            foreach (string sId in storagesIds)
             {
                 Console.WriteLine("StorageId: " + sId);
             }
@@ -78,96 +77,5 @@ namespace WorkerNode
             return new DIDAWorker.DIDAVersion { VersionNumber = res.VersionNumber, ReplicaId = res.ReplicaId };
         }
 
-        class ConsistentHashing {
-
-            private int RING_SIZE = 256;
-            private List<HashStorageNode> storageHashIds = new List<HashStorageNode>();
-            private int numOfServers;
-            private int replicationFactor = 3;
-            private Random rng = new Random();
-
-            public ConsistentHashing(List<string> storageKeys)
-            {
-                numOfServers = storageKeys.Count;
-                int storageIdx = 1;
-                foreach (string key in storageKeys)
-                {
-                    this.storageHashIds.Add(new HashStorageNode
-                    {
-                        serverId = key,
-                        hashUpperBound = storageIdx == numOfServers ? RING_SIZE : NodeHash(storageIdx)
-                    });
-                    storageIdx++;
-                }
-                this.display();
-            }
-
-            public void display()
-            {
-                foreach (HashStorageNode node in storageHashIds)
-                {
-                    Console.WriteLine("StorageId: " + node.serverId);
-                    Console.WriteLine("StorageHash: " + node.hashUpperBound);
-                }
-            }
-
-            public int NodeHash(int idx)
-            {
-                return (int) (idx * Math.Floor((double) (RING_SIZE / numOfServers)));
-            }
-
-            public int KeyHash(string key)
-            {
-                // Create a SHA256   
-                using (SHA256 sha256Hash = SHA256.Create())
-                {
-                    // ComputeHash - returns byte array  
-                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(key));
-                    // We only need the last byte (bytes mod 256) == byte[-1]
-                    return bytes[bytes.Length - 1];
-                }
-            }
-
-            public List<string> ComputeSetOfReplicas(string key)
-            {
-                int keyHash = KeyHash(key);
-                List<string> setOfReplicas = new List<string>();
-                bool found = false;
-
-                for (int i = 0; setOfReplicas.Count < replicationFactor; i = (i+1) % storageHashIds.Count)
-                {
-                    if (keyHash < storageHashIds[i].hashUpperBound && !found)
-                    {
-                        setOfReplicas.Add(storageHashIds[i].serverId);
-                        found = true;
-                    } else if (found)
-                    {
-                        setOfReplicas.Add(storageHashIds[i].serverId);
-                    }
-                }
-
-                return Shuffle(setOfReplicas);
-            }
-
-            public List<string> Shuffle(List<string> list)
-            {
-                int n = list.Count;
-                while (n > 1)
-                {
-                    n--;
-                    int k = rng.Next(n + 1);
-                    string value = list[k];
-                    list[k] = list[n];
-                    list[n] = value;
-                }
-                return list;
-            }
-        }
-
-        public struct HashStorageNode
-        {
-            public string serverId;
-            public int hashUpperBound;
-        }
     }
 }
